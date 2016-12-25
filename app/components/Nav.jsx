de4e12383d66bdefebfae1bfe7d6c3cce7ba59d4
Linas <linas.petrenas@gmail.com>
2016-12-25 17:51:29 +0200

    Add Nav search suport

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -4,7 +4,14 @@ var {Link, IndexLink} = require('react-router');
 var Nav = React.createClass({
     onSearch: function(e) {
         e.preventDefault();
-        alert("Not yet wired up");
+
+        var location = this.refs.search.value;
+        var encodedLocation = encodeURIComponent(location);
+
+        if (location.length > 0) {
+            this.refs.search.value = '';
+            window.location.hash = '#/?location=' + encodedLocation;
+        }
     },
     render: function() {
         return (
@@ -13,19 +20,13 @@ var Nav = React.createClass({
                     <ul className="menu">
                         <li className="menu-text">React Weather App</li>
                         <li>
-                            <IndexLink to="/" activeClassName="active" activeStyle={{
-                                fontWeight: 'bold'
-                            }}>Get Weather</IndexLink>
+                            <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
                         </li>
                         <li>
-                            <Link to="/about" activeClassName="active" activeStyle={{
-                                fontWeight: 'bold'
-                            }}>About</Link>
+                            <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
                         </li>
                         <li>
-                            <Link to="/examples" activeClassName="active" activeStyle={{
-                                fontWeight: 'bold'
-                            }}>Examples</Link>
+                            <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
                         </li>
                     </ul>
                 </div>
@@ -33,7 +34,7 @@ var Nav = React.createClass({
                     <form onSubmit={this.onSearch}>
                         <ul className="menu">
                             <li>
-                                <input type="search" placeholder="Search weather by city"/>
+                                <input type="search" placeholder="Search weather by city" ref="search"/>
                             </li>
                             <li>
                                 <input type="submit" className="button" value="Get Weather"/>
